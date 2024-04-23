a9ec3d19853a33dff9c414493fba41448d6d075b
Chris <chriswfoster@gmail.com>
2024-04-22 21:34:28 -0500

    done with another socket client test

--- index.js
+++ index.js
@@ -20,11 +20,6 @@ var server = http.createServer(function (request, response) {
 
 wsServer = new WebSocketServer({
     httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
     autoAcceptConnections: false
 });
 
@@ -36,6 +31,7 @@ app.get('/data', (req, res) => {
     res.status(200).send(solarData);
 });
 
+let solarData = {};
 
 function originIsAllowed(origin) {
     // put logic here to detect whether the specified origin is allowed.
@@ -43,21 +39,14 @@ function originIsAllowed(origin) {
 }
 
 wsServer.on('request', function (request) {
-    // console.log("request is happening", request)
-    // console.log(request)
     if (!originIsAllowed(request.origin)) {
-        // Make sure we only accept requests from an allowed origin
         request.reject();
         console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
         return;
     }
     var connection = request.accept(null, request.origin)
-
     connection.on('message', function (message) {
-        console.log("Message in request: ", message)
         if (message.type === 'utf8') {
-            console.log("yeah its utf8")
-            console.log('Received Message: ', message.utf8Data);
             solarData = JSON.parse(message.utf8Data);
 
             //connection.sendUTF(message.utf8Data); this resend the reseived message, instead of it i will send a custom message. hello from nodejs
@@ -70,87 +59,63 @@ wsServer.on('request', function (request) {
             connection.sendBytes(message.binaryData);
         }
     });
+    connection.on('close', function(reasonCode, description) {
+        console.log('Client disconnected:', reasonCode, description);
+        // Perform cleanup or handle reconnection here
+    });
 })
 
-wsServer.on('connection', function connection(ws) {
+// async function start() {
+//     try {
+//         console.log(new Date().toISOString() + "Starting server connection...");
+//         connectToServer();
+//     }
+//     catch (e) {
+//         console.log('Socket Error: ', e);
+//         console.log("Restarting connection in 5 seconds...");
+//         // setTimeout(connectToServer, 5000);
+//         process.exit(1);
+//     }
+// }
 
-    console.log('Client connected to WebSocket client.');
+// const getTime = () => {
+//     const date = new Date();
+//     const hour = date.getHours();
+//     const minute = date.getMinutes();
+//     const formattedHour = hour < 10 ? "0" + hour : hour;
+//     const formattedMinute = minute < 10 ? "0" + minute : minute;
+//     const timeString = `${formattedHour}:${formattedMinute}`;
 
-    ws.on('message', (message) => {
-        console.log("message received!")
-        if (message.type === 'utf8') {
-            console.log("Message in connectioN: ", message)
-            // console.log(new Date().toISOString() + 'Received message:', JSON.parse(message.utf8Data));
-            // solarData = JSON.parse(message.utf8Data);
-            solarData.shop_time = getTime();
-        } else {
-            console.log(message);
-        }
-    });
+//     return timeString;
+// }
 
-    console.log('A new client connected!');
+// const connectToServer = () => {
+//     try {
+//         const client = new WebSocket.client();
 
-    // ws.on('message', function incoming(message) {
-    //   console.log('Received: %s', message);
-    // });
+//         client.on('connect', (connection) => {
+//             console.log('Client connected to WebSocket server.');
 
-    // ws.send('Hello, client!');
-});
+//             connection.on('message', (message) => {
+//                 if (message.type === 'utf8') {
+//                     console.log(new Date().toISOString() + 'Received message:', JSON.parse(message.utf8Data));
+//                     solarData = JSON.parse(message.utf8Data);
+//                     solarData.shop_time = getTime();
+//                 }
+//             });
 
+//         });
 
-let solarData = {};
-
-async function start() {
-    try {
-        console.log(new Date().toISOString() + "Starting server connection...");
-        connectToServer();
-    }
-    catch (e) {
-        console.log('Socket Error: ', e);
-        console.log("Restarting connection in 5 seconds...");
-        // setTimeout(connectToServer, 5000);
-        process.exit(1);
-    }
-}
-
-const getTime = () => {
-    const date = new Date();
-    const hour = date.getHours();
-    const minute = date.getMinutes();
-    const formattedHour = hour < 10 ? "0" + hour : hour;
-    const formattedMinute = minute < 10 ? "0" + minute : minute;
-    const timeString = `${formattedHour}:${formattedMinute}`;
-
-    return timeString;
-}
-
-const connectToServer = () => {
-    try {
-        const client = new WebSocket.client();
-
-        client.on('connect', (connection) => {
-            console.log('Client connected to WebSocket server.');
-
-            connection.on('message', (message) => {
-                if (message.type === 'utf8') {
-                    console.log(new Date().toISOString() + 'Received message:', JSON.parse(message.utf8Data));
-                    solarData = JSON.parse(message.utf8Data);
-                    solarData.shop_time = getTime();
-                }
-            });
-
-        });
-
-        client.on('connectFailed', (error) => {
-            console.error(new Date().toISOString() + 'Connection failed:', error.toString());
-            throw 'Connection failed';
-        });
-        client.connect(url);
-    }
-    catch (e) {
-        throw e;
-    }
-}
+//         client.on('connectFailed', (error) => {
+//             console.error(new Date().toISOString() + 'Connection failed:', error.toString());
+//             throw 'Connection failed';
+//         });
+//         client.connect(url);
+//     }
+//     catch (e) {
+//         throw e;
+//     }
+// }
 
 process.on('uncaughtException', (error) => {
     console.error('Uncaught exception:', error);
